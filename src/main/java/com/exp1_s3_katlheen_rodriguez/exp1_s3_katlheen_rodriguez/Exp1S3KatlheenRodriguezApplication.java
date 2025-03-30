2886ed269bc70cd34aa18c765138f66ad303342e
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2025-03-29 22:57:08 -0300

    Proyecto sPRING

---
+++ src/main/java/com/exp1_s3_katlheen_rodriguez/exp1_s3_katlheen_rodriguez/Exp1S3KatlheenRodriguezApplication.java
@@ -0,0 +1,13 @@
+package com.exp1_s3_katlheen_rodriguez.exp1_s3_katlheen_rodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp1S3KatlheenRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp1S3KatlheenRodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp1_s3_katlheen_rodriguez/exp1_s3_katlheen_rodriguez/Exp1S3KatlheenRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp1_s3_katlheen_rodriguez.exp1_s3_katlheen_rodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp1S3KatlheenRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
